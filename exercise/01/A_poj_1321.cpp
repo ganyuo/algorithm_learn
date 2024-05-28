1d6a8be2ca260b57e7c2dcad52ced3a58ada3578
ganyuo <ganyuo@outlook.com>
2024-05-28 10:10:47 +0800

    practice restart

--- exercise/01/A_poj_1321.cpp
+++ exercise/01/A_poj_1321.cpp
@@ -1,62 +1,110 @@
-/* AC */
 #include <stdio.h>
 #include <string.h>
 
-const int MAXN = 10;
+const int MAXN = 20;
 
+struct node_t
+{
+    int x, y;
+};
+
+int n, k, ans;
 char mp[MAXN][MAXN];
-int col_book[MAXN];
+int node_num;
+node_t nodes[MAXN * MAXN];
+char row_v[MAXN], col_v[MAXN];
 
-int dfs(int i, int n, int k)
+void dfs(int start, int count)
 {
-	int j, ans;
-	if(k == 0)
-	{
-		return 1;
-	}
-	ans = 0;
-	for(j = 0; j < n; j++)
-	{
-		if(mp[i][j] == '#' && col_book[j] == 0)
-		{
-			col_book[j] = 1;
-			ans += dfs(i + 1, n, k - 1);
-			col_book[j] = 0;
-		}
-	}
-	if(n - i >= k)
-	{
-		ans += dfs(i + 1, n, k);
-	}
-	return ans;
+    if(count == k)
+    {
+        ans++;
+        return;
+    }
+    for(int i = start; i < node_num; i++)
+    {
+        if(row_v[nodes[i].x] == 0 && col_v[nodes[i].y] == 0)
+        {
+            row_v[nodes[i].x] = 1;
+            col_v[nodes[i].y] = 1;
+            dfs(i + 1, count +  1);
+            row_v[nodes[i].x] = 0;
+            col_v[nodes[i].y] = 0;
+        }
+    }
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-	int n, k;
-	int i, ans;
+    while(scanf("%d %d", &n, &k) != EOF)
+    {
+        if(n < 0 || k < 0)
+        {
+            break;
+        }
+        node_num = 0;
+        // memset(mp, 0, sizeof(mp));
+        for(int i = 0; i < n; i++)
+        {
+            scanf("%s", mp[i]);
+            for(int j = 0; j < n; j++)
+            {
+                if(mp[i][j] == '#')
+                {
+                    nodes[node_num].x = i;
+                    nodes[node_num].y = j;
+                    node_num++;
+                }
+            }
+        }
+        if(k > node_num || k > n)
+        {
+            printf("0\n");
+            continue;
+        }
 
-	while(scanf("%d%d", &n, &k) != EOF)
-	{
-		getchar();
-		if(n == -1 && k == -1)
-		{
-			break;
-		}
-		// if(k == 0)
-		// {
-		// 	printf("1\n");
-		// 	continue;
-		// }
-		memset(mp, '.', sizeof(mp));
-		memset(col_book, 0, sizeof(col_book));
-		for(i = 1; i <= n; i++)
-		{
-			fgets(mp[i], MAXN, stdin);
-		}
-		ans = 0;
-		ans += dfs(1, n, k);
-		printf("%d\n", ans);
-	}
-	return 0;
+        ans = 0;
+        memset(row_v, 0, sizeof(row_v));
+        memset(col_v, 0, sizeof(col_v));
+        dfs(0, 0);
+        printf("%d\n", ans);
+    }
+
+    return 0;
 }
+
+/*
+Sample Input
+2 1
+#.
+.#
+4 4
+...#
+..#.
+.#..
+#...
+4 2
+...#
+..#.
+.#..
+#...
+4 4
+.#.#
+..#.
+.#..
+#...
+4 5
+.#.#
+..#.
+.#..
+#...
+-1 -1
+
+Sample Output
+2
+1
+6
+1
+0
+
+*/
