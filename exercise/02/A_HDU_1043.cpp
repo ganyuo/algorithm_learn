7ac636067191f3db55fa7d3742f83c3f549caf3f
ganyuo <ganyuo@outlook.com>
2024-06-03 13:37:09 +0800

    优化了01_A_HDU_1043的时间

--- exercise/02/A_HDU_1043.cpp
+++ exercise/02/A_HDU_1043.cpp
@@ -1,4 +1,5 @@
-/* time limit exceeded */
+/* https://acm.hdu.edu.cn/showproblem.php?pid=1043 */
+/* oj暂时提交不上 */
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
@@ -6,9 +7,8 @@
 const int MAX_N = 20;
 const int MAX_M = 362880 + 10;
 
-struct bfs_node_t
+struct step_node_t
 {
-    int position;
     int pre;
     char pre_operation;
     unsigned char x_index;
@@ -57,108 +57,24 @@ void reverse_contor_expansion(int64_t position, int len, unsigned char sequence[
     }
 }
 
-bfs_node_t que[MAX_M];
-char book[MAX_M];
-char operations[MAX_M];
+step_node_t step[MAX_M];
 
-int bfs(unsigned char start[])
+int bfs()
 {
     unsigned char sequ_buff[MAX_N];
-    int operation_len;
+    int32_t que[MAX_M];
     int front = 0, back = 1;
-    
-    memset(book, 0, sizeof(book));
-    que[0].position = contor_expansion(start, 9);
-    que[0].pre = -1;
-    que[0].pre_operation = '0';
-    for(int i = 0; i < 9; i++)
-    {
-        if(start[i] == 9)
-        {
-            que[0].x_index = i;
-            break;
-        }
-    }
-    if(que[0].position == 0)
-    {
-        operations[0] = '\0';
-        return 0;
-    }
-    book[que[0].position] = 1;
+
+    memset(step, -1, sizeof(step));
+    step[0].pre = -1;
+    step[0].pre_operation = 0;
+    step[0].x_index = 8;
+    que[0] = 0;
     while(front < back)
     {
-        bfs_node_t temp = que[front];
-        reverse_contor_expansion(temp.position, 9, sequ_buff);
-        if(temp.x_index >= 3 && temp.pre_operation != 'd') /* up */
-        {
-            int exchange_index = temp.x_index - 3;
-            unsigned char temp_slid = sequ_buff[exchange_index];
-            sequ_buff[exchange_index] = 9;
-            sequ_buff[temp.x_index] = temp_slid;
-            int pos = contor_expansion(sequ_buff, 9);
-            sequ_buff[exchange_index] = temp_slid;
-            sequ_buff[temp.x_index] = 9;
-            if(book[pos] == 0)
-            {
-                que[back].position = pos;
-                que[back].pre = front;
-                que[back].pre_operation = 'u';
-                que[back].x_index = exchange_index;
-                back++;
-                book[pos] = 1;
-                if(pos == 0)
-                {
-                    break;
-                }
-            }
-        }
-        if(temp.x_index < 6 && temp.pre_operation != 'u') /* down */
-        {
-            int exchange_index = temp.x_index + 3;
-            unsigned char temp_slid = sequ_buff[exchange_index];
-            sequ_buff[exchange_index] = 9;
-            sequ_buff[temp.x_index] = temp_slid;
-            int pos = contor_expansion(sequ_buff, 9);
-            sequ_buff[exchange_index] = temp_slid;
-            sequ_buff[temp.x_index] = 9;
-            if(book[pos] == 0)
-            {
-                que[back].position = pos;
-                que[back].pre = front;
-                que[back].pre_operation = 'd';
-                que[back].x_index = exchange_index;
-                back++;
-                book[pos] = 1;
-                if(pos == 0)
-                {
-                    break;
-                }
-            }
-        }
-        if(temp.x_index % 3 != 0 && temp.pre_operation != 'r') /* left */
-        {
-            int exchange_index = temp.x_index - 1;
-            unsigned char temp_slid = sequ_buff[exchange_index];
-            sequ_buff[exchange_index] = 9;
-            sequ_buff[temp.x_index] = temp_slid;
-            int pos = contor_expansion(sequ_buff, 9);
-            sequ_buff[exchange_index] = temp_slid;
-            sequ_buff[temp.x_index] = 9;
-            if(book[pos] == 0)
-            {
-                que[back].position = pos;
-                que[back].pre = front;
-                que[back].pre_operation = 'l';
-                que[back].x_index = exchange_index;
-                back++;
-                book[pos] = 1;
-                if(pos == 0)
-                {
-                    break;
-                }
-            }
-        }
-        if(temp.x_index % 3 != 2 && temp.pre_operation != 'l') /* right */
+        step_node_t temp = step[que[front]];
+        reverse_contor_expansion(que[front], 9, sequ_buff);
+        if(temp.x_index % 3 != 2 && temp.pre_operation != 'r') /* right */
         {
             int exchange_index = temp.x_index + 1;
             unsigned char temp_slid = sequ_buff[exchange_index];
@@ -167,40 +83,71 @@ int bfs(unsigned char start[])
             int pos = contor_expansion(sequ_buff, 9);
             sequ_buff[exchange_index] = temp_slid;
             sequ_buff[temp.x_index] = 9;
-            if(book[pos] == 0)
+            if(step[pos].pre_operation == -1)
             {
-                que[back].position = pos;
-                que[back].pre = front;
-                que[back].pre_operation = 'r';
-                que[back].x_index = exchange_index;
+                step[pos].pre = que[front];
+                step[pos].pre_operation = 'l';
+                step[pos].x_index = exchange_index;
+                que[back] = pos;
                 back++;
-                book[pos] = 1;
-                if(pos == 0)
-                {
-                    break;
-                }
+            }
+        }
+        if(temp.x_index < 6 && temp.pre_operation != 'd') /* down */
+        {
+            int exchange_index = temp.x_index + 3;
+            unsigned char temp_slid = sequ_buff[exchange_index];
+            sequ_buff[exchange_index] = 9;
+            sequ_buff[temp.x_index] = temp_slid;
+            int pos = contor_expansion(sequ_buff, 9);
+            sequ_buff[exchange_index] = temp_slid;
+            sequ_buff[temp.x_index] = 9;
+            if(step[pos].pre_operation == -1)
+            {
+                step[pos].pre = que[front];
+                step[pos].pre_operation = 'u';
+                step[pos].x_index = exchange_index;
+                que[back] = pos;
+                back++;
+            }
+        }
+        if(temp.x_index % 3 != 0 && temp.pre_operation != 'l') /* left */
+        {
+            int exchange_index = temp.x_index - 1;
+            unsigned char temp_slid = sequ_buff[exchange_index];
+            sequ_buff[exchange_index] = 9;
+            sequ_buff[temp.x_index] = temp_slid;
+            int pos = contor_expansion(sequ_buff, 9);
+            sequ_buff[exchange_index] = temp_slid;
+            sequ_buff[temp.x_index] = 9;
+            if(step[pos].pre_operation == -1)
+            {
+                step[pos].pre = que[front];
+                step[pos].pre_operation = 'r';
+                step[pos].x_index = exchange_index;
+                que[back] = pos;
+                back++;
+            }
+        }
+        if(temp.x_index >= 3 && temp.pre_operation != 'u') /* up */
+        {
+            int exchange_index = temp.x_index - 3;
+            unsigned char temp_slid = sequ_buff[exchange_index];
+            sequ_buff[exchange_index] = 9;
+            sequ_buff[temp.x_index] = temp_slid;
+            int pos = contor_expansion(sequ_buff, 9);
+            sequ_buff[exchange_index] = temp_slid;
+            sequ_buff[temp.x_index] = 9;
+            if(step[pos].pre_operation == -1)
+            {
+                step[pos].pre = que[front];
+                step[pos].pre_operation = 'd';
+                step[pos].x_index = exchange_index;
+                que[back] = pos;
+                back++;
             }
         }
         front++;
     }
-
-    if(front < back)
-    {
-        int j = MAX_M;
-        for(int i = back - 1; que[i].pre >= 0; i = que[i].pre)
-        {
-            j--;
-            operations[j] = que[i].pre_operation;
-        }
-        operation_len = MAX_M - j;
-        for(int i = 0; i < operation_len; i++)
-        {
-            operations[i] = operations[i + j];
-        }
-        operations[operation_len] = '\0';
-        return operation_len;
-    }
-    return -1;
 }
 
 int main()
@@ -214,6 +161,7 @@ int main()
     {
         factorial[i] = factorial[i - 1] * i;
     }
+    bfs();
 
     while (true)
     {
@@ -237,16 +185,17 @@ int main()
                 return 0;
             }
         }
-
-        operation_len = bfs(start);
-        if(operation_len < 0)
+        int pos = contor_expansion(start, 9);
+        if(step[pos].pre_operation == -1)
         {
-            printf("unsolvable\n");
+            printf("unsolved");
         }
-        else
+        while (pos > 0)
         {
-            printf("%s\n", operations);
+            printf("%c", step[pos].pre_operation);
+            pos = step[pos].pre;
         }
+        printf("\n");
     }
 
     return 0;
@@ -262,5 +211,7 @@ x  8  7  6  5  4  3  2  1
 
 output:
 ullddrurdllurdruldr
+dlurulldrrdllurdruldr
+ddrruullddrruullddrruullddrr
 
 */
