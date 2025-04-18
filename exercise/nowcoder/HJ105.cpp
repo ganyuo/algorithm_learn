241f1e20808446457293a42f4add18601e2fe97a
ganyuo <ganyuo@outlook.com>
2025-04-18 17:43:58 +0800

    更新线段树

--- exercise/nowcoder/HJ105.cpp
+++ exercise/nowcoder/HJ105.cpp
@@ -14,18 +14,53 @@ struct node_t
 
 char str[2][MAX_N];
 node_t tree[2][MAX_N * 4];
+int k;
+
+void init_node(int root, int ind)
+{
+    memset(&tree[k][root], 0, sizeof(node_t));
+    if(str[k][ind] == 'r')
+    {
+        tree[k][root].r = 1;
+    }
+    else if(str[k][ind] == 'e')
+    {
+        tree[k][root].e = 1;
+    }
+    else if(str[k][ind] == 'd')
+    {
+        tree[k][root].d = 1;
+    }
+}
+
+void update_node(int root, int left, int right)
+{
+
+}
+
+void build(int root, int left, int right)
+{
+    if (right - left == 1)
+    {
+        init_node(root, left);
+    }
+    int mid = (left + right) / 2;
+    build(root * 2, left, mid);
+    build(root * + 1, mid, right);
+    update_node(root, root * 2, root * 2 + 1);
+}
 
 int main()
 {
     int n, q;
     char sub[10] = "0red";
-    
+
     scanf("%d %d", &n, &q);
     scanf("%s %s", str[0] + 1, str[1] + 1);
 
     while (q--)
     {
-        printf("%lld\n", tree[0][0].red - tree[1][0].red);
+        printf("%lld\n", tree[0][1].red - tree[1][1].red);
     }
     return 0;
 }
